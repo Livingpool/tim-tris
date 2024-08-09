e8400010ce12b9a33e81ba1cb9a090e30b6f1213
Livingpool <52132459+Livingpool@users.noreply.github.com>
2024-08-09 22:02:46 +0800

    add renderer

---
+++ handler/handler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Livingpool/tim-tris/web"
+)
+
+type HttpHandler struct {
+	renderer web.TemplatesInterface
+}
+
+func NewHttpHandler(renderer web.TemplatesInterface) *HttpHandler {
+	return &HttpHandler{
+		renderer: renderer,
+	}
+}
+
+func (h *HttpHandler) Home(w http.ResponseWriter, r *http.Request) {
+	h.renderer.Render(w, "home", nil)
+}
+
+func (h *HttpHandler) Chat(w http.ResponseWriter, r *http.Request) {
+	h.renderer.Render(w, "chat", nil)
+}
+
+func (h *HttpHandler) Room(w http.ResponseWriter, r *http.Request) {
+	h.renderer.Render(w, "chat", nil)
+}
